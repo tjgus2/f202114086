9c3343ff0b92926b61412740acfe9b437d81cd23
Park, Seohyun <tjgus6279@naver.com>
2024-10-16 09:38:32 +0900

    five

--- src/f202114086/World.java
+++ src/f202114086/World.java
@@ -3,6 +3,10 @@ public class World {
     public static void run() {
       System.out.println("202114086");
     }
+    public static void run2() {
+        System.out.println("hello world");
+      }
+    
   
   }
   
